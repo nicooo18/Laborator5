3ac13bf4d76fe91c795a5772e5fc60e6574306a1
nicobrezai <nicoletastoica1022@yahoo.com>
2025-03-27 09:15:11 +0200

    Al doilea commit

--- src/Application.java
+++ src/Application.java
@@ -7,20 +7,34 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         try {
+            System.out.println("Original file content:");
+            printSmallTextFile("in.txt");
+
             System.out.println("\n\n");
-            printAndModifyFile("in.txt");
+            printWithNewlineAfterDot("in.txt");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    static void printAndModifyFile(String fileName) throws IOException {
+    static void printSmallTextFile(String fileName) throws IOException {
+        System.out.println("Using Files.readAllLines:");
+        Path path = Paths.get(fileName);
+        List<String> allLinesInMemory = Files.readAllLines(path);
+        for (String line : allLinesInMemory) {
+            System.out.println(": " + line);
+        }
+    }
+
+    static void printWithNewlineAfterDot(String fileName) throws IOException {
+        System.out.println("Reading file and adding newline after '.' :");
         Path path = Paths.get(fileName);
         List<String> lines = Files.readAllLines(path);
 
         for (String line : lines) {
-            System.out.print(": " + line + "\n\n");
+            String modifiedLine = line.replace(".", ".\n"); // Adaugă un newline după fiecare '.'
+            System.out.print(": " + modifiedLine + "\n");
         }
     }
 }
